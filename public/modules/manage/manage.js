340e60e2927ec073b723ed84c301ce6ee96a3bf2
zhangdousang <zhangdousang@yeah.net>
2017-02-20 21:42:47 +0800

    导航条优化

--- public/modules/manage/manage.js
+++ public/modules/manage/manage.js
@@ -7,6 +7,9 @@ export default class Students extends React.Component{
         this.state={
             current:"users",
             collapsed:false,
+            mode:'inline',
+            spanWords:"inline-block",
+            IconPic:"16px"
         }
     }
     ComponentWillMount(){
@@ -18,18 +21,32 @@ export default class Students extends React.Component{
         })
         this.props.router.replace(e.key)
     }
-    render(){       
+    onCollapse(collapsed){
+        this.setState({
+         collapsed,
+         mode: collapsed ? 'vertical' : 'inline',
+         spanWords: collapsed ? 'none' : 'inline-block',
+         IconPic: collapsed ? '24px' : '16px',
+        });
+    }
+    render(){
+       var spanCss={
+            display:this.state.spanWords
+        }
+        var IconCss={
+           fontSize:this.state.IconPic
+        }
         return <Layout>
-            <Sider collapsible >
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['users']} onClick={this.handleClick.bind(this)} selectedKeys={[this.state.current]} >
-            <Menu.Item key="users"><Icon type="team" />用户管理</Menu.Item>
-            <Menu.Item key="informations"><Icon type="user" />资讯管理</Menu.Item>
-            <Menu.Item key="films"><Icon type="solution" />电影管理</Menu.Item>
-            <Menu.Item key="screenings"><Icon type="solution" />院线管理</Menu.Item>
-            <Menu.Item key="filmAndScreen"><Icon type="solution" />电影院线匹配管理</Menu.Item>
-            <Menu.Item key="show"><Icon type="solution" />正在热映管理</Menu.Item>
-            <Menu.Item key="notshow"><Icon type="solution" />即将上映管理</Menu.Item>
-            <Menu.Item key="hot"><Icon type="solution" />热播电影管理</Menu.Item>
+            <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse.bind(this)}>
+            <Menu theme="dark" mode={this.state.mode} defaultSelectedKeys={['users']} onClick={this.handleClick.bind(this)} selectedKeys={[this.state.current]} >
+            <Menu.Item key="users"><Icon style={IconCss} type="team" /><span style={spanCss}>用户管理</span></Menu.Item>
+            <Menu.Item key="informations"><Icon style={IconCss} type="book" /><span style={spanCss}>资讯管理</span></Menu.Item>
+            <Menu.Item key="films"><Icon style={IconCss} type="video-camera" /><span style={spanCss}>电影管理</span></Menu.Item>
+            <Menu.Item key="screenings"><Icon style={IconCss} type="scan" /><span style={spanCss}>院线管理</span></Menu.Item>
+            <Menu.Item key="filmAndScreen"><Icon style={IconCss} type="switcher" /><span style={spanCss}>电影院线匹配管理</span></Menu.Item>
+            <Menu.Item key="show"><Icon style={IconCss} type="area-chart" /><span style={spanCss}>正在热映管理</span></Menu.Item>
+            <Menu.Item key="notshow"><Icon style={IconCss} type="dot-chart" /><span style={spanCss}>即将上映管理</span></Menu.Item>
+            <Menu.Item key="hot"><Icon style={IconCss} type="bar-chart" /><span style={spanCss}>热播电影管理</span></Menu.Item>
             </Menu>
             </Sider>
             <Content style={{minHeight:"460px"}}>
